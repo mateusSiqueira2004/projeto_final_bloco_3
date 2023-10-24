83a7b4a72a317fb25aa2cd7f4fe02c43b1a16198
Mateus Siqueira Salomão <118471492+mateusSiqueira2004@users.noreply.github.com>
2023-10-24 09:18:43 -0300

    Contruindo melhor o CSS

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,27 +1,33 @@
 import React from 'react'
-import '../../Home.css'
+import './Home.css'
 
 function Home() {
-  return (
-    <>
-            <div className="CorpoPrincipal">
-                <div>
-                    <div className="CorpoTitulos">
-                        <h2>Seja Bem Vinde a pagina Inicial!</h2>
-                        <p>Pagina com um monte de coisas mesmo sendo vazia</p>
-                    </div>
-
-                    <div className="CorpoImagem" >
-                        <img 
-                            src="https://i.imgur.com/VpwApCU.png" 
-                            alt="Imagem da Página Home" 
-                            width="400px"
-                        />
-                    </div>
+    return (
+        <>
+            <header>
+                <h1 className='NavTitulo'>BlogPessoal</h1>
+                <div className='NavBar'>
+                    <label className='NavItens'>Home</label>
+                    <label className='NavItens'>Postagens</label>
                 </div>
-            </div>
+            </header>
+            <main>
+                <div className="CorpoTexto">
+                    <h2>Seja Bem Vinde a pagina Inicial!</h2>
+                    <p>Enquanto aprendia Java, meu lema era "o esforço de hoje é fracasso do amanhã". <br/>Agora meu lema é "A tentativa leva ao erro...<br/>E o erro leva a correção, e da correção nasce a conclusão"<br />Este é apenas um blog para falar sobre mim sinta-se livre para me conhecer 😊</p>
+                    <button className='ButtonHome'>Seja Bem-Vinde!</button>
+                </div>
+
+                <div className="CorpoImagem" >
+                    <img
+                        src="https://i.imgur.com/VpwApCU.png"
+                        alt="Imagem da Página Home"
+                        className='imgHome'
+                    />
+                </div>
+            </main>
         </>
-  )
+    )
 }
 
 export default Home
